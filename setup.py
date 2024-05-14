3eb9be69bc77f312d65916ffefa0afbed1bc8b2b
armanddidierjean <95971503+armanddidierjean@users.noreply.github.com>
2024-05-14 16:19:50 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="helloasso_api_wrapper",
+    version="0.0.1",
+    description="Python wrapper for Helloasso APIV5",
+    url="https://github.com/HelloAsso/HaApiV5",
+    author="ECLAIR",
+    maintainer="ECLAIR",
+    license="MIT License",
+    packages=find_packages(exclude=("tests", "docs")),
+    install_requires=[
+        "helloasso-apiv5==1.0.0",
+        "pydantic==2.5.0",
+    ],
+    python_requires=">=3.6",
+)
